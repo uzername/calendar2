6419809b249239778db136230b00050e09e41afb
uzername <ivanpost777@gmail.com>
2018-03-18 23:59:51 +0200

    works

--- ConsoleAppCalendar/CodeHandling.cs
+++ ConsoleAppCalendar/CodeHandling.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using SunSetRiseLib;
 using Xceed.Words.NET;
 
 namespace ConsoleAppCalendar
@@ -20,7 +21,7 @@ namespace ConsoleAppCalendar
             documentName = "MyDocument.docx";
             pageWidth = CodeHandling.cmToPoints(29.7f);
             pageHeight = CodeHandling.cmToPoints(21.0f);
-            numcolsTable = 3; numrowsTable = 2;
+            numcolsTable = 3; numrowsTable = 3;
         }
     }
     public class CodeHandling
@@ -43,6 +44,31 @@ namespace ConsoleAppCalendar
         public static float cmToPoints(float in_cmValue) {
             return in_cmValue / ((float)2.54 / (float)72.0);
         }
+        /// <summary>
+        /// http://pointofint.blogspot.com/2014/06/sunrise-and-sunset-in-c.html
+        /// </summary>
+        /// <param name="latitude"> More than 0 if northern lat</param>
+        /// <param name="longitude"> More than 0 if eastern long</param>
+        /// <returns></returns>
+        public static Tuple<string,string> getSunsetAndSunRise(Boolean useDayLightSaveTime, int timezoneCorr, System.DateTime date, double latitude, double longitude)
+        {
+            double JD = Util.calcJD(date);  //OR   JD = Util.calcJD(2014, 6, 1);
+            double sunRise = Util.calcSunRiseUTC(JD, latitude, longitude);
+            double sunSet = Util.calcSunSetUTC(JD, latitude, longitude);
+            string sunrisetimesrtr = Util.getTimeString(sunRise, timezoneCorr, JD, useDayLightSaveTime);
+            string sunsettimesrtr = Util.getTimeString(sunSet, timezoneCorr, JD, useDayLightSaveTime);
+            return new Tuple<string, string>(sunrisetimesrtr, sunsettimesrtr);
+        }
+        
+
+        /// <summary>
+        /// convert cm to pixels by multiplying points value on 2/3
+        /// </summary>
+        /// <param name="in_cmValue"></param>
+        /// <returns></returns>
+        public static float cmToPixels(float in_cmValue) {
+            return cmToPoints(in_cmValue) * ((float)2.0 / (float)3.0);
+        }
         public void testCreateDocument() {
             DocX document = DocX.Create("Indentation.docx");
             // Add a title.
@@ -59,10 +85,13 @@ namespace ConsoleAppCalendar
             {
                 document.PageHeight = in_documentArgs.pageHeight;
                 document.PageWidth = in_documentArgs.pageWidth;
-                document.MarginBottom = cmToPoints(1.0f); document.MarginTop = cmToPoints(1.0f); document.MarginLeft = cmToPoints(1.0f); document.MarginRight = cmToPoints(1.0f);
+                float btmMargin = 1.0f; float topMargin = 1.0f; float leftMargin = 1.0f; float rightMargin = 1.0f;
+                document.MarginBottom = cmToPoints(btmMargin); document.MarginTop = cmToPoints(topMargin); document.MarginLeft = cmToPoints(leftMargin); document.MarginRight = cmToPoints(rightMargin);
                 Table insertedTable = document.InsertTable(in_documentArgs.numrowsTable, in_documentArgs.numcolsTable);
                 Border b = new Border(BorderStyle.Tcbs_single, BorderSize.one, 0, Color.Blue);
-
+                //calculate each column width
+                float bestColumnWidth = cmToPixels( ((float)in_documentArgs.pageWidth - (float)(leftMargin + rightMargin)) / (float)(in_documentArgs.numcolsTable) );
+                float bestRowHeightPt = ( ((float)in_documentArgs.pageHeight - (float)(topMargin + btmMargin)) / (float)(in_documentArgs.numcolsTable) );
                 // Set the tables Top, Bottom, Left and Right Borders to b.
                 insertedTable.SetBorder(TableBorderType.Top, b);
                 insertedTable.SetBorder(TableBorderType.Bottom, b);
@@ -72,7 +101,23 @@ namespace ConsoleAppCalendar
                 insertedTable.SetBorder(TableBorderType.InsideV, b);
                 byte currentRow = 0; byte currentCol = 0;
                 while ((currentRow < in_documentArgs.numrowsTable) && (currentCol < in_documentArgs.numcolsTable)&& (theCurrentDate <= date2)) {
-                    insertedTable.Rows[currentRow].Cells[currentCol].InsertParagraph(String.Format("{0:yyyy MMMM dd}", theCurrentDate));
+                    Paragraph yearMonthP = insertedTable.Rows[currentRow].Cells[currentCol].InsertParagraph(String.Format("{0:yyyy, MMMM}", theCurrentDate));
+                    Paragraph dayNumberP = insertedTable.Rows[currentRow].Cells[currentCol].InsertParagraph(String.Format("{0:dd}", theCurrentDate));
+                    Paragraph weekdayP = insertedTable.Rows[currentRow].Cells[currentCol].InsertParagraph(String.Format("{0:dddd}", theCurrentDate));
+                    yearMonthP.Alignment = Alignment.center; yearMonthP.Font("Courier New");
+                    dayNumberP.Alignment = Alignment.center; dayNumberP.Font("Courier New"); dayNumberP.FontSize(15); dayNumberP.Bold();
+                    weekdayP.Alignment = Alignment.center; weekdayP.Font("Courier New");
+
+                    Tuple<string, string> sunTimes = getSunsetAndSunRise(true, 2, theCurrentDate, 49.4444, 32.0597);
+                    insertedTable.Rows[currentRow].Cells[currentCol].InsertParagraph(String.Format("{0} : {1}", sunTimes.Item1, sunTimes.Item2));
+
+
+                    insertedTable.Rows[currentRow].Cells[currentCol].Paragraphs[0].Remove(false);
+                    insertedTable.SetColumnWidth(currentCol, bestColumnWidth);
+                    // 100*2/3pt -> 3.53 cm
+                    // x  px -> bestRowHeightCm
+                    insertedTable.Rows[currentRow].Height = Math.Round(bestRowHeightPt*0.88);
+
                     theCurrentDate = theCurrentDate.AddDays(1.0);
                     currentCol++;
                     if ((currentCol >= in_documentArgs.numcolsTable)&&(currentRow<in_documentArgs.numrowsTable-1)) {
@@ -80,12 +125,13 @@ namespace ConsoleAppCalendar
                     }
                 }
                 if (theCurrentDate <= date2) {
-                    document.InsertSectionPageBreak();
+                    //document.InsertSectionPageBreak();
+                    insertedTable.InsertPageBreakAfterSelf();
                 }
             } while (theCurrentDate <= date2);
             
             
-            document.InsertTable(in_documentArgs.numrowsTable, in_documentArgs.numcolsTable);
+            //document.InsertTable(in_documentArgs.numrowsTable, in_documentArgs.numcolsTable);
             document.Save();
         }
     }
